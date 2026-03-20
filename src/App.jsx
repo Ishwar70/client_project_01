e935ea908f4c1c4fbdbff5d7a2b6d029f5b66b7d
abhisheksingh01-ai <abhishek.ai.learn@gmail.com>
2026-03-20 15:44:10 +0530

    Details

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Packages from './components/package/Packages';
 import WorldDestinationsExplorer from './pages/Worlddestinationsexplorer';
+// import Destinationdetailpage from "./Destinationdetailpage";
 
 function App() {
   return (
